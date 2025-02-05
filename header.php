a1bec44a39f7782169361da1a2f569676462966b
Jhun Lorenz Asna <jlorenzaasna@gmail.com>
2025-02-06 00:55:04 +0800

    Adding and Deleting Files
    Deleted Unnecessary Files from Underscore and Replacing "Partials"

--- header.php
+++ header.php
@@ -21,6 +21,14 @@
 </head>
 
 <body <?php body_class(); ?>>
+
+<?php
+
+get_template_part('partials/part', 'two');
+
+?>
+
+
 <?php wp_body_open(); ?>
 <div id="page" class="site">
 	<a class="skip-link screen-reader-text" href="#primary"><?php esc_html_e( 'Skip to content', 'geniuscourses' ); ?></a>
